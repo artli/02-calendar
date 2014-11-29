130fb43e1a8314b4c2065ba22d93de2e02ec40ce
Arthur Liss <artli@e1.ru>
2014-11-30 00:13:07 +0500

    Create the project

---
+++ Calendar/CalendarPageGenerator/Generator.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace CalendarPageGenerator {
+    public class Generator {
+        public Bitmap GeneratePage(DateTime date) {
+            return new Bitmap(1, 1);
+        }
+    }
+}
